f501b3362fdb57ba54aa5635fe47831109f6a49d
13angs <13angz.um@gmail.com>
2022-06-04 09:11:21 +0700

    using parallel request

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { NotificationService } from './api/api';
 
 const service = new NotificationService();
@@ -6,5 +7,25 @@ async function fetchData() {
   const res = await service.put();
   console.log(res.data);
 }
+
+async function fetchWeather() {
+  try {
+    const api1 = axios.get('https://api.testing.winonahappyv.net');
+    const api2 = axios.get('https://api.testing.winonahappyv.net');
+
+    const res = await Promise.all([api1, api2]);
+    console.log(res);
+    // console.log(res[0].data);
+    // console.log(res[1].data);
+
+    if (res[0].status === 200 && res[1].status === 200) {
+      console.log('Both pass');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fetchWeather();
 
 // fetchData();
